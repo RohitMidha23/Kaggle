3b7d59f06faad70b4d13c3d6a5e76b4c35b8cc7e
Rohit Midha <rohit.midha23@gmail.com>
2019-07-24 17:32:55 +0530

    updated to resnet152

--- OpenImages/inference.py
+++ OpenImages/inference.py
@@ -173,14 +173,14 @@ if __name__ == '__main__':
     # files_to_process = glob.glob(INPUT_PATH + 'kaggle/challenge2018_test/*.jpg')
     files_to_process = glob.glob(DATASET_PATH + '*.jpg')
 
-    if 1:
+    if 0:
         backbone = 'resnet101'
         pretrained_model_path = MODELS_PATH + 'retinanet_resnet101_level_1_v1.2_converted.h5'
         labels_list = LEVEL_1_LABELS
 
-    if 0:
+    if 1:
         backbone = 'resnet152'
-        pretrained_model_path = MODELS_PATH + 'retinanet_resnet152_level_1_converted.h5'
+        pretrained_model_path = MODELS_PATH + 'retinanet_resnet152_level_1_v1.2_converted.h5'
         labels_list = LEVEL_1_LABELS
 
     output_cache_directory = OUTPUT_PATH + 'cache_retinanet_level_1_{}/'.format(backbone)
